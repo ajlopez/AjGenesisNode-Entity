ccecb987dba674ceaa682137d0ad0b77fc7c990b
ajlopez <webmaster@ajlopez.com>
2014-01-01 10:11:16 -0300

    New tests for names

---
+++ test/names.js
@@ -0,0 +1,33 @@
+
+var names = require('../lib/names');
+
+exports['Get name'] = function (test) {
+    test.equal(names.getName('name'), 'name');
+    test.equal(names.getName('Name'), 'name');
+    test.equal(names.getName('NAME'), 'name');
+};
+
+exports['Get name from null'] = function (test) {
+    test.equal(names.getName(null), null);
+};
+
+exports['Get descriptor'] = function (test) {
+    test.equal(names.getDescriptor('name'), 'Name');
+    test.equal(names.getDescriptor('Name'), 'Name');
+    test.equal(names.getDescriptor('NAME'), 'NAME');
+};
+
+exports['Get descriptor from null'] = function (test) {
+    test.equal(names.getDescriptor(null), null);
+};
+
+exports['Get plural'] = function (test) {
+    test.equal(names.getPlural('name'), 'names');
+    test.equal(names.getPlural('Customer'), 'Customers');
+    test.equal(names.getPlural('city'), 'cities');    
+    test.equal(names.getPlural('suppliers'), 'suppliers');
+};
+
+exports['Get plural for null'] = function (test) {
+    test.equal(names.getPlural(null), null);
+};
