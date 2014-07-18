241f60548fc7aa50204ffc0cebe731b08635640d
ajlopez <webmaster@ajlopez.com>
2014-07-18 17:07:54 -0300

    import task, WIP

---
+++ import.js
@@ -0,0 +1,21 @@
+
+var path = require('path'),
+    names = require('./lib/names');
+
+module.exports = function (model, args, ajgenesis, cb) {
+    var model = ajgenesis.loadModel(args[0]);
+    
+    if (!model || !model.entities || !Array.isArray(model.entities)) {
+        cb(null, null);
+        return;
+    }
+        
+    for (var n in model.entities) {
+        var entity = model.entities[n];
+        var entitymodel = { entities: [ entity ] };
+        ajgenesis.saveModel(entity.name, entitymodel);
+    }
+    
+    cb(null, null);
+}
+
